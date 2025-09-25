c37196136db7f8d5a660fead4052fbe9433cc579
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2025-09-25 10:09:49 +0530

    final

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,10 +1,10 @@
 // 🧠 Schema definitions
 import z from "zod"
-export const signupInput = z.object({
-  email: z.string().email(),
-  password: z.string(),
-  name: z.string().optional()
-});
+  export const signupInput = z.object({
+    email: z.string().email(),
+    password: z.string(),
+    name: z.string().optional()
+  });
 
 export const signinInput = z.object({
   email: z.string().email(),
